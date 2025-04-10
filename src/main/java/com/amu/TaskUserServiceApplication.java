1956414dbb1456d1ca768d7983335bb956a1cff9
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-04-10 15:24:36 +0700

    Regiter Service Client on Eureka Serve

--- src/main/java/com/amu/TaskUserServiceApplication.java
+++ src/main/java/com/amu/TaskUserServiceApplication.java
@@ -2,8 +2,9 @@ package com.amu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TaskUserServiceApplication {
 
